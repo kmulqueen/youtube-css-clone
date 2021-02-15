7490cabff7e8142936d31472992b7c8c12a5c8b6
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-02-15 13:27:59 -0700

    Added markup and styles for the see responses and hide responses buttons that are attached to comments with responses. Added javascript to show responses and hide responses when the buttons are clicked.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const downTickButton = document.querySelector(".comment-downtick");
 const upTickButton = document.querySelector(".comment-uptick");
 const commentListContainer = document.querySelector(".comment-list-container");
+const seeReponseButtons = document.querySelectorAll(
+  ".see-response-container button"
+);
+const hideResponseButtons = document.querySelectorAll(
+  ".hide-response-container button"
+);
 
 // Show comments section
 downTickButton.addEventListener("click", () => {
@@ -14,4 +20,31 @@ upTickButton.addEventListener("click", () => {
   downTickButton.style.display = "block";
   upTickButton.style.display = "none";
   commentListContainer.style.display = "none";
+});
+
+// Show comment responses
+seeReponseButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const container = button.parentElement;
+    const responseList = container.nextElementSibling;
+    const hideResponseButton = container.nextElementSibling.nextElementSibling;
+
+    container.style.display = "none";
+    hideResponseButton.style.display = "grid";
+    responseList.style.display = "block";
+  });
+});
+
+// Hide comment responses
+hideResponseButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const container = button.parentElement;
+    const responseList = container.previousElementSibling;
+    const showResponsesButton =
+      container.previousElementSibling.previousElementSibling;
+
+    container.style.display = "none";
+    responseList.style.display = "none";
+    showResponsesButton.style.display = "grid";
+  });
 });
